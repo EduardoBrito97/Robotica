cfed6cb44edd5132dedb6b1d45d865cc730de912
Unknown <jnds@cin.ufpe.br>
2021-03-13 19:12:43 -0300

    Update motion control

--- Projeto1/Projeto1.cpp
+++ Projeto1/Projeto1.cpp
@@ -67,24 +67,32 @@ inline double to_deg(double radians) {
 
 void controle_movimento(simxFloat pos[3], simxFloat goal[3], simxFloat* PhiL, simxFloat* PhiR)
 {    
-    double deltaX,deltaY,ro,alpha,beta,v,w;
+    double deltaX,deltaY,ro,alpha,beta,v,w,transCmRad;
     double Kp,Ka,Kb;
 
     Kp = 3.0;
-    Ka = 8.0;
+    Ka = 5.0;
     Kb = -1.5;
 
     deltaX = goal[0] - pos[0];
     deltaY = goal[1] - pos[1];
     ro = sqrt( (deltaX*deltaX) + (deltaY*deltaY));
     alpha = -pos[2] + atan2(deltaY,deltaX);
-    beta =  -pos[2] - alpha;
+    beta = -pos[2] - alpha;
 
     v = Kp*ro;
     w = (Ka*alpha) + (Kb*beta);
 
-    *PhiR = v + ((L*w)/2);
-    *PhiL = v - ((L*w)/2);
+    // Tentar dar ré
+    if (alpha <= (-M_PI/2) || alpha > (M_PI / 2)) {
+        v = -v;
+        w = -w;
+    }
+
+    *PhiR =( v + ((L*w)/2))*10;
+    *PhiL =(v - ((L*w)/2))*10;
+
+    printf("V e w: [%.2f %.2f ]",v ,w);
 } 
 
 int main(int argc, char* argv[]) {
